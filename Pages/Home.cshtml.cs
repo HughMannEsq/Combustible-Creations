e47c5d17d336b7affc6b71b8ac3033f25f32591b
Ross <officialrm@gmx.com>
2025-07-08 22:09:42 -0400

    Home model change

--- Pages/Home.cshtml.cs
+++ Pages/Home.cshtml.cs
@@ -1,12 +1,19 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace MyWebsite.Pages
+namespace AutumnRidgeUSA.Pages
 {
     public class HomeModel : PageModel
     {
         public void OnGet()
         {
+            // This method runs when the page is loaded via GET request
+            // Add any initialization logic here
+        }
+
+        public void OnPost()
+        {
+            // This method runs when the page is submitted via POST request
+            // Add any form processing logic here
         }
     }
 }
